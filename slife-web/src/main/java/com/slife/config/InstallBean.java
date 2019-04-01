7683ff705268470602d559bb0f562f0eb5e5aaca
felixu <xufei_0320@163.com>
2019-04-01 17:16:18 +0800

    去掉不必要配置与无用代码

--- slife-web/src/main/java/com/slife/config/InstallBean.java
+++ slife-web/src/main/java/com/slife/config/InstallBean.java
@@ -1,6 +1,7 @@
 package com.slife.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -8,22 +9,25 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by chenjianan on 2016/12/16-11:25.
- * <p>
- * Describe:
  * 如果我们需要在Spring容器完成Bean的实例化，配置和其他的初始化后添加一些自己的逻辑处理，
  * 我们就可以定义一个或者多个BeanPostProcessor接口的实现。
+ *
+ * @author jamen
+ * @date 2016.12.16
  */
-@Configuration
+@Slf4j
+//@Configuration
 public class InstallBean implements BeanPostProcessor {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        logger.info("对象" + beanName + "开始实例化");
+        log.info("对象" + beanName + "开始实例化");
         return bean;
     }
 
+    @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        logger.info("对象" + beanName + "实例化完成");
+        log.info("对象" + beanName + "实例化完成");
         return bean;
     }
 }
--- slife-web/src/main/java/com/slife/config/Setup.java
+++ slife-web/src/main/java/com/slife/config/Setup.java
@@ -1,20 +1,24 @@
 package com.slife.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by chenjianan on 2016/12/15-17:24.
- * <p>
- * Describe:
  * beanFactoryPostprocessor的作用是在beanFactory初始化之后提供一个修改的机会。
  * 在Spring实例化bean的前后执行一些附加操作
+ *
+ * @author jamen
+ * @date 2016.12.15
  */
-@Configuration
+@Slf4j
+//@Configuration
 public class Setup implements BeanFactoryPostProcessor {
+
+    @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        System.out.println("beanFactoryPostprocessor的作用是在beanFactory初始化之后提供一个修改的机会。......这时候bean 还没实例化（instance)");
+        log.info("beanFactoryPostprocessor的作用是在beanFactory初始化之后提供一个修改的机会。......这时候bean 还没实例化（instance)");
     }
 }
